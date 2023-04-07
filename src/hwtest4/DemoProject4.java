499e4fbc6704f9adaae9fd6346ef1c7cbc40df73
Abdessalam <Abdessalam@192.168.148.117>
2023-04-07 20:11:48 +0000

    fourth commit

---
+++ src/hwtest4/DemoProject4.java
@@ -0,0 +1,12 @@
+package hwtest4;
+
+public class DemoProject4 {
+	public static void main(String[] args) {
+		
+		System.out.println(" this is demo project 4");
+		
+		
+		System.out.println("this a project 4");
+	}
+
+}
